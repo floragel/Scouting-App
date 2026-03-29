0dc904066a19eeac1a3eebe9a4ee5dc4ff7dec1a
floragel <floragel@2024@@gmail.com>
2026-03-28 20:00:45 -0400

    Import missing 'request' in app.py

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import cloudinary
 import cloudinary.uploader
